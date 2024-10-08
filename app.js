236e4332d22586763b65cb0a6a473bff126fcbe1
wuffenbauer <wuffenbauer@gmail.com>
2024-10-08 21:18:43 +0700

    struktur folder aplikasi

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Project Baru')
+const express   = require('express')
+const app       = express()
+const port      = 3000
+const c_beranda = require('./controller/c_beranda')
+
+app.use(express.urlencoded({extended:false}))
+app.set('view engine', 'ejs')
+app.set('views', './view')
+
+app.get('/', c_beranda.halaman_awal)
+
+app.listen(port, () => {
+    console.log(`Server aktif, buka http://localhost:${port}`)
+})
---
+++ controller/c_beranda.js
@@ -0,0 +1,5 @@
+module.exports = {
+    halaman_awal: function(req, res) {
+        res.render('beranda')
+    }
+}
