7faad3b39be12dbdb113bfb828ea1452973728ec
wuffenbauer <wuffenbauer@gmail.com>
2024-10-10 20:22:47 +0700

    auth login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express   = require('express')
 const app       = express()
 const port      = 3000
 const c_beranda = require('./controller/c_beranda')
+const c_auth    = require('./controller/c_auth')
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
@@ -10,6 +11,7 @@ app.set('view engine', 'ejs')
 app.set('views', './view')
 
 app.get('/', c_beranda.halaman_awal)
+app.get('/auth/login', c_auth.halaman_login)
 
 app.listen(port, () => {
     console.log(`Server aktif, buka http://localhost:${port}`)
---
+++ controller/c_auth.js
@@ -0,0 +1,5 @@
+module.exports = {
+    halaman_login: function(req, res) {
+        res.render('v_auth/login')
+    }
+}
