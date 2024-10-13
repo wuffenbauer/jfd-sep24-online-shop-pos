d583a0b0b584318c5947231bbbbf05b383bd9c88
wuffenbauer <wuffenbauer@gmail.com>
2024-10-13 16:31:19 +0700

    perbaikan bugs setelah login

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const express   = require('express')
-const app       = express()
-const port      = 3000
-const c_beranda = require('./controller/c_beranda')
-const c_auth    = require('./controller/c_auth')
-const c_toko    = require('./controller/c_toko')
+const express       = require('express')
+const app           = express()
+const port          = 3000
+const cookieParser  = require('cookie-parser')
+const session       = require('express-session')
+const c_beranda     = require('./controller/c_beranda')
+const c_auth        = require('./controller/c_auth')
+const cek_login     = c_auth.cek_login
+const c_toko        = require('./controller/c_toko')
 
+// setting-an untuk data session login
+app.use(cookieParser('rahasia'))
+app.use(session({
+    secret: 'rahasia',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: (1000* 60) * 30
+        // batas session expired:
+        // 1000 milidetik * 60 = 1 menit
+        // 1 menit * 30 = 1/2 jam
+    }
+}))
 app.use(express.urlencoded({extended:false}))
 app.use(express.static('public'))
 
@@ -15,7 +31,7 @@ app.get('/', c_beranda.halaman_awal)
 app.get('/auth/login', c_auth.halaman_login)
 app.post('/auth/proses-login', c_auth.proses_login)
 
-app.get('/toko', c_toko.halaman_utama)
+app.get('/toko', cek_login, c_toko.halaman_utama)
 
 app.listen(port, () => {
     console.log(`Server aktif, buka http://localhost:${port}`)
--- controller/c_auth.js
+++ controller/c_auth.js
@@ -3,18 +3,22 @@ const model_user    = require('../model/m_user')
 
 module.exports = {
     halaman_login: function(req, res) {
-        let data = {
-            notification: req.query.notification,
+        if (req.session.user) {
+            res.redirect('/toko')
+        } else {
+            let data = {
+                notification: req.query.notification,
+            }
+            res.render('v_auth/login', data)
         }
-        res.render('v_auth/login', data)
     },
 
     proses_login: async function(req, res) {
         // ambil inputan dari halaman login
-        let form_email    = req.body.email
-        let form_password = req.body.password
+        let form_email    = req.body.form_email
+        let form_password = req.body.form_password
         
-        // cek email yang diinput, ada ngga di database
+        // cek email yang diinput, ada ngga di db
         let email_exist   = await model_user.cari_email(form_email)
         // console.log(email)
         
@@ -23,17 +27,28 @@ module.exports = {
             let password_cocok = bcrypt.compareSync(form_password, email_exist[0].password)
             if (password_cocok) {
                 // arahkan ke halaman utama sistem
+                req.session.user = email_exist
                 res.redirect('/toko')
             } else {
                 // tendang ke halaman login
                 let pesan = `Password salah`
                 res.redirect(`/auth/login?notification=${pesan}`)
             }
-            res.send('Password cocok')
         } else {
             // tendang ke halaman register
-            let pesan = `Email Anda belum terdaftar. Silakan registrasi lebih dulu.`
+            let pesan = `Email Anda belum terdaftar, silakan registrasi lebih dulu.`
             res.redirect(`/auth/login?notification=${pesan}`)
         }
     },
+
+    cek_login: function(req, res, next) {
+        if (req.session.user) {
+            next()
+        } else {
+            let pesan = `Sesi Anda sudah habis, silakan login lagi.`
+            res.redirect(`/auth/login?notification=${pesan}`)
+        }
+    },
+
+
 }
