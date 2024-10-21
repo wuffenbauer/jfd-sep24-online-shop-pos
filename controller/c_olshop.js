034024db13e7ae7c06f97fd03ffcd92580141e34
wuffenbauer <wuffenbauer@gmail.com>
2024-10-21 20:42:27 +0700

    upload foto produk

--- controller/c_olshop.js
+++ controller/c_olshop.js
@@ -15,7 +15,7 @@ module.exports = {
         let data = {
             kategoriProduk: await m_prod_kategori.getAll(),
             produkJual    : await m_master_produk.getAll(),
-            // notification  : req.query.notification,
+            notification  : req.query.notification,
         }
         res.render('v_olshop/produk/index', data)
     },
@@ -28,16 +28,23 @@ module.exports = {
     },
 
     proses_insert_produk: async function(req, res) {
-        let foto            = req.files.form_foto1
+        let foto1           = req.files.form_foto1
 
         // ganti nama file asli
         let email           = req.session.user[0].email.replaceAll('.','-')
-        let dateTime        = moment().format('YYYMMDD_HHmmss')
-        let file_name       = username + '_' + dateTime + '-' + foto1.name
-        let folder_simpan   = path.join(__dirname, '../public/upload/produk-foto')
+        let dateTime        = moment().format('YYYYMMDD_HHmmss')
+        let filename_foto1  = email + '_' + dateTime + '_' + foto1.name
+        let folder_simpan   = path.join(__dirname, '../public/upload/produk-foto', filename_foto1)
 
         try {
-            let insert = await m_master_produk.insert(req)
+            // pakai function mv() untuk meletakkan file di suatu folder/direktori
+            foto1.mv(folder_simpan, async function(err) {
+                if (err) {
+                    return res.status(500).send(err)
+                }
+            })
+
+            let insert = await m_master_produk.insert(req, filename_foto1)
             if (insert.affectedRows > 0) {
                 res.redirect('/olshop/produk?notification=Berhasil input produk baru')
             }
